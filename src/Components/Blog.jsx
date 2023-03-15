ecf0508cb91fbad54f7a233a6f2e64853d25d777
himanshu20163 <109578416+himanshu20163@users.noreply.github.com>
2023-03-15 22:55:32 +0530

    Update Blog.jsx

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Blog = () => {
   return (
     <div><h1>Blog.</h1>
-    <img src="./images/blogbanner.webp" alt="" style={{width:"100%"}} />
+    <img src="images/blogbanner.webp" alt="" style={{width:"100%"}} />
     </div>
   )
 }
